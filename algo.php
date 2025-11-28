2ae3b53cc0f142fc97cfd7a731e64fbe62a7f1e3
DBouraoui <contact@dbouraoui.fr>
2025-11-28 15:55:16 +0100

    Implement string reversal from command line input
    Add functionality to reverse input string from command line

--- algo.php
+++ algo.php
@@ -1,3 +1,16 @@
 <?php
 
-return "test";
+if ($argc < 2) {
+    echo "Erreur : aucun argument fourni.\n";
+    exit(1);
+}
+
+$input = $argv[1];
+$reversed = "";
+
+// Boucle de la fin vers le début
+for ($i = strlen($input) - 1; $i >= 0; $i--) {
+    $reversed .= $input[$i];
+}
+
+echo $reversed . "\n";
